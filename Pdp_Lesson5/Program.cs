d9ecc332e68252710d64cbc6175fb45dae92b77f
Abror <abrorhalimov660@gmail.com>
2025-02-22 16:56:27 +0500

    Add project files.

---
+++ Pdp_Lesson5/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Pdp_Lesson5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter lenght: ");
+            //int lengh = int.Parse(Console.ReadLine());
+            //var digits = AddMassiv(lengh);
+            //Logic(digits);
+            //FillGap(digits);
+
+            ///////////////////////////////////////
+            var GetClass = new Task2();
+
+            //Console.Write("Engter Lengh");
+            //int lengh = int.Parse(Console.ReadLine());
+            //var numbers = GetClass.AddMassiv(lengh);
+            //GetClass.OddOrEven(numbers);
+
+            //////////////////////////////////////////
+
+            //int[][] array = new int[4][]
+            //{
+            //    [23,21,32,12,42],
+            //    [3,71,32,2,32],
+            //    [9,21,91,12,32],
+            //    [61,21,11,12,52],
+
+            //};
+            //int Odd = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = 0; j < array[i].Length; j++)
+            //    {
+            //        if (array[i][j] % 2 == 1)
+            //        {
+            //            Odd++;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(Odd);
+
+            
+
+        }
+
+
+        public static List<int> AddMassiv(int lengh)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < lengh; i++)
+            {
+                Console.Write($"{i}: ");
+                int elements = int.Parse(Console.ReadLine());
+                numbers.Add(elements);
+            }
+
+            return numbers;
+        }
+
+        public static void Logic(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    numbers[i] *= 2;
+                }
+            }
+        }
+
+        public static void FillGap(List<int> numbers)
+        {
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }
+}
---
+++ Pdp_Lesson5/Task2.cs
@@ -0,0 +1,54 @@
+﻿namespace Pdp_Lesson5
+{
+    public class Task2
+    {
+        public List<int> AddMassiv(int massiv)
+        {
+            var numbers = new List<int>();
+            for (int i = 0; i < massiv; i++)
+            {
+                Console.Write($"{i}:");
+                int elements = int.Parse(Console.ReadLine());
+                numbers.Add(elements);
+            }
+            return numbers;
+        }
+        public void Logic(List<int> massiv)
+        {
+            int count = 0;
+            for (int i = 0; i < massiv.Count; i++)
+            {
+                if (massiv[i] >= 10 && massiv[i] <= 99)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+        public void OddOrEven(List<int> massiv)
+        {
+            int odd = 0;
+            int even = 0;
+            for (int i = 0; i < massiv.Count; i++)
+            {
+                if (massiv[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+
+            if (odd > even)
+            {
+                Console.WriteLine("Toq son");
+            }
+            else
+            {
+                Console.WriteLine("Juft son");
+            }
+        }
+    }
+}
